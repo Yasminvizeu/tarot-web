109c72cd23a040c45b97593a4183142c6746cc32
Yasmin <vizeugilyasmin@gmail.com>
2024-06-12 17:32:50 -0300

    finalizando JogarTarot()

--- js/main.js
+++ js/main.js
@@ -1,34 +1,48 @@
-const openModal = () => {
+var cartas = [
+    {
+        name:'Seven of Cups',
+        value_int:7,
+        type:'minor',
+        meaning_up:'Fairy favours, images of reflection, sentiment, imagination, things seen in the glass of contemplation; some attainment in these degrees, but nothing permanent or substantial is suggested.',meaning_rev:'Desire, will, determination, project',
+        desc:'Strange chalices of vision, but the images are more especially those of the fantastic spirit.'
+    },
+    {
+        name:'Nine of Cups',
+        value_int:7,
+        type:'minor',
+        meaning_up:'Fairy favours, images of reflection, sentiment, imagination, things seen in the glass of contemplation; some attainment in these degrees, but nothing permanent or substantial is suggested.',meaning_rev:'Desire, will, determination, project.',desc:'Strange chalices of vision, but the images are more especially those of the fantastic spirit.'
+    },
+    {
+        name:'Five of wands',
+        value_int:7,
+        type:'minor',
+        meaning_up:'Fairy favours, images of reflection, sentiment, imagination, things seen in the glass of contemplation; some attainment in these degrees, but nothing permanent or substantial is suggested.',meaning_rev:'Desire, will, determination, project.',desc:'Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.'
+    },
+    
+];
+const openModalJogar = () => {
 	const modal = document.getElementById('jogar-cartas-modal')
 	modal.style.display = 'flex'
+}
+
+const openModalListar = () => {
+	const modal = document.getElementById('listar-cartas-modal')
+	modal.style.display = 'flex'
+}
+
+function fecharModalJogar() {
+    var modal = document.getElementById("jogar-cartas-modal");
+    modal.style.display = "none";
+}
+function fecharModalListar() {
+    var modal = document.getElementById("listar-cartas-modal");
+    modal.style.display = "none";
 }
 
 function jogarTarot(event) {
     event.preventDefault(); // Prevenir o comportamento padrão do formulário
     
     // Nomes e descrições das cartas 
-    var cartas = [
-        {
-            name:'Seven of Cups',
-            value_int:7,
-            type:'minor',
-            meaning_up:'Fairy favours, images of reflection, sentiment, imagination, things seen in the glass of contemplation; some attainment in these degrees, but nothing permanent or substantial is suggested.',meaning_rev:'Desire, will, determination, project',
-            desc:'Strange chalices of vision, but the images are more especially those of the fantastic spirit.'
-        },
-        {
-            name:'Nine of Cups',
-            value_int:7,
-            type:'minor',
-            meaning_up:'Fairy favours, images of reflection, sentiment, imagination, things seen in the glass of contemplation; some attainment in these degrees, but nothing permanent or substantial is suggested.',meaning_rev:'Desire, will, determination, project.',desc:'Strange chalices of vision, but the images are more especially those of the fantastic spirit.'
-        },
-        {
-            name:'Five of wands',
-            value_int:7,
-            type:'minor',
-            meaning_up:'Fairy favours, images of reflection, sentiment, imagination, things seen in the glass of contemplation; some attainment in these degrees, but nothing permanent or substantial is suggested.',meaning_rev:'Desire, will, determination, project.',desc:'Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.'
-        },
-        
-    ];
     
     // Selecionar aleatoriamente uma carta
     var cartaSelecionada = cartas[Math.floor(Math.random() * cartas.length)];
@@ -52,9 +66,4 @@ function jogarTarot(event) {
     // // Exibir o modal
     // var modal = document.getElementById("jogar-cartas-modal");
     // modal.style.display = "none"; // Esconder o modal após a seleção da carta
-}
-
-function fecharModal() {
-    var modal = document.getElementById("jogar-cartas-modal");
-    modal.style.display = "none";
 }
