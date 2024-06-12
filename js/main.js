04206144409b57932237108d0d249ca0b93c0e92
Yasmin <vizeugilyasmin@gmail.com>
2024-06-12 17:09:52 -0300

    jogarCarta()

--- js/main.js
+++ js/main.js
@@ -8,9 +8,25 @@ function jogarTarot(event) {
     
     // Nomes e descrições das cartas 
     var cartas = [
-        { nome: "Ás de Copas", descricao: "A carta do amor e das emoções profundas." },
-        { nome: "Rei de Espadas", descricao: "A carta da autoridade e do poder intelectual." },
-        { nome: "Sete de Paus", descricao: "A carta da defesa e da luta." }
+        {
+            name:'Seven of Cups',
+            value_int:7,
+            type:'minor',
+            meaning_up:'Fairy favours, images of reflection, sentiment, imagination, things seen in the glass of contemplation; some attainment in these degrees, but nothing permanent or substantial is suggested.',meaning_rev:'Desire, will, determination, project',
+            desc:'Strange chalices of vision, but the images are more especially those of the fantastic spirit.'
+        },
+        {
+            name:'Nine of Cups',
+            value_int:7,
+            type:'minor',
+            meaning_up:'Fairy favours, images of reflection, sentiment, imagination, things seen in the glass of contemplation; some attainment in these degrees, but nothing permanent or substantial is suggested.',meaning_rev:'Desire, will, determination, project.',desc:'Strange chalices of vision, but the images are more especially those of the fantastic spirit.'
+        },
+        {
+            name:'Five of wands',
+            value_int:7,
+            type:'minor',
+            meaning_up:'Fairy favours, images of reflection, sentiment, imagination, things seen in the glass of contemplation; some attainment in these degrees, but nothing permanent or substantial is suggested.',meaning_rev:'Desire, will, determination, project.',desc:'Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.Strange chalices of vision, but the images are more especially those of the fantastic spirit.'
+        },
         
     ];
     
@@ -18,11 +34,20 @@ function jogarTarot(event) {
     var cartaSelecionada = cartas[Math.floor(Math.random() * cartas.length)];
 
     // Concatenar o nome da pessoa com a descrição da carta
-    var descricaoCompleta = event.target.nome.value + ", voce tirou a carta " + cartaSelecionada.nome;
+    var saudacao ="Hi " + event.target.nome.value + ", you got the card: " + cartaSelecionada.name;
+    var valor ="VALUE:" + cartaSelecionada.value_int;
+    var tipo ="TYPE: Arcan " + cartaSelecionada.type;
+    var meaningUp ="MEANING UP: " + cartaSelecionada.meaning_up;
+    var meaningRev ="MEANING REV: " + cartaSelecionada.meaning_rev;
+    var descricao ="DESCRIPTION: " + cartaSelecionada.desc;
     
     // Exibir o nome e a descrição da carta selecionada
-    document.getElementById("nome-carta").textContent = descricaoCompleta;
-    document.getElementById("descricao-carta").textContent = cartaSelecionada.descricao;
+    document.getElementById("nome-carta").textContent = saudacao;
+    document.getElementById("valor-carta").textContent = valor;
+    document.getElementById("tipo-carta").textContent = tipo;
+    document.getElementById("meaning-up-carta").textContent = meaningUp;
+    document.getElementById("meaning-rev-carta").textContent = meaningRev;
+    document.getElementById("descricao-carta").textContent = descricao;
     
     // // Exibir o modal
     // var modal = document.getElementById("jogar-cartas-modal");
